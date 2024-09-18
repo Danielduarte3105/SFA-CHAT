9c691e3941f859419a90e007309458d37c36a0e4
Daniel Duarte | Dev FrontEnd | Java <84358066+Danielduarte3105@users.noreply.github.com>
2024-09-18 12:01:43 -0300

    Update server.js

--- server.js
+++ server.js
@@ -19,8 +19,15 @@ const io = require('socket.io')(http)
 
 io.on('connection', (socket) => {
     console.log('Connected...')
-    socket.on('message', (msg) => {
-        socket.broadcast.emit('message', msg)
-    })
 
+    // Adicionar o usuário a uma sala com base no nome
+    socket.on('joinRoom', (room) => {
+        socket.join(room);
+        console.log(`User joined room: ${room}`);
+    });
+
+    socket.on('message', (msg) => {
+        const room = msg.room; // A sala para a qual a mensagem será enviada
+        io.to(room).emit('message', msg);
+    })
 })
